935cfe4205917148226d80a618dd5cedbf3844ea
nathantioe <nat21220@uga.edu>
2022-11-21 10:18:17 -0500

    Added login/logout with firebase

--- app/src/main/java/edu/uga/cs/roommateshoppingapp/MainActivity.java
+++ app/src/main/java/edu/uga/cs/roommateshoppingapp/MainActivity.java
@@ -1,11 +1,23 @@
 package edu.uga.cs.roommateshoppingapp;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,38 +27,76 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button login = findViewById(R.id.buttonLogin);
-        login.setOnClickListener(new ButtonClickListener());
+        login.setOnClickListener(new SignInButtonClickListener());
 
-        Button registrer = findViewById(R.id.buttonRegistrer);
-        registrer.setOnClickListener(new ButtonClickListener());
+        Button register = findViewById(R.id.buttonRegistrer);
+        register.setOnClickListener(new RegisterButtonClickListener());
 
     }
-}
 
-class ButtonClickListener implements
-        View.OnClickListener
-{
-    /**
-     *
-     * @param view View in which the buttons are
-     */
-    @Override
-    public void onClick( View view ) {
-        if (view.getId() == R.id.buttonRegistrer ) {
-            Intent intent = new
-                    Intent(view.getContext(),
-                    RegistrerActivity.class);
-            String message = "";
-            intent.putExtra("MESSAGE", message);
-            view.getContext().startActivity(intent);
-        } if (view.getId() == R.id.buttonLogin ) {
-            Intent intent = new
-                    Intent(view.getContext(),
-                    MainMenuActivity.class);
-            String message = "";
-            intent.putExtra("MESSAGE", message);
+    private class RegisterButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            // start the user registration activity
+            Intent intent = new Intent(view.getContext(), RegisterActivity.class);
             view.getContext().startActivity(intent);
         }
+    }
 
+    // A button listener class to start a Firebase sign-in process
+    private class SignInButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick( View v ) {
+            // This is an example of how to use the AuthUI activity for signing in to Firebase.
+            // Here, we are just using email/password sign in.
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build()
+            );
+
+            // Create an Intent to singin to Firebese.
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build();
+            signInLauncher.launch(signInIntent);
+        }
+    }
+
+    // The ActivityResultLauncher class provides a new way to invoke an activity
+    // for some result.  It is a replacement for the deprecated method startActivityForResult.
+    //
+    // The signInLauncher variable is a launcher to start the AuthUI's logging in process that
+    // should return to the MainActivity when completed.  The overridden onActivityResult
+    // is then called when the Firebase logging-in process is finished.
+    private ActivityResultLauncher<Intent> signInLauncher =
+            registerForActivityResult(
+                    new FirebaseAuthUIActivityResultContract(),
+                    new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                        @Override
+                        public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                            onSignInResult(result);
+                        }
+                    }
+            );
+
+    // This method is called once the Firebase sign-in activity (launched above) returns (completes).
+    // Then, the current (logged-in) Firebase user can be obtained.
+    // Subsequently, there is a transition to the MainMenuActivity.
+    private void onSignInResult( FirebaseAuthUIAuthenticationResult result ) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+
+            // after a successful sign in, start the MainMenuActivity
+            Intent intent = new Intent( this, MainMenuActivity.class );
+            startActivity( intent );
+        }
+        else {
+            // Sign in failed. If response is null the user canceled the
+            Toast.makeText( getApplicationContext(),
+                    "Sign in failed",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
--- app/src/main/java/edu/uga/cs/roommateshoppingapp/MainMenuActivity.java
+++ app/src/main/java/edu/uga/cs/roommateshoppingapp/MainMenuActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -14,38 +15,43 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         Button recentBuys = findViewById(R.id.buttonRecentBuys);
-        recentBuys.setOnClickListener(new ButtonClickListener2());
+        recentBuys.setOnClickListener(new ButtonClickListener());
 
         Button shoppingList = findViewById(R.id.buttonShoppingList);
-        shoppingList.setOnClickListener(new ButtonClickListener2());
+        shoppingList.setOnClickListener(new ButtonClickListener());
+
+        Button logout = findViewById(R.id.buttonLogout);
+        logout.setOnClickListener(new ButtonClickListener());
 
     }
-}
 
-class ButtonClickListener2 implements
-        View.OnClickListener
-{
-    /**
-     *
-     * @param view View in which the buttons are
-     */
-    @Override
-    public void onClick( View view ) {
-        if (view.getId() == R.id.buttonRecentBuys ) {
-            Intent intent = new
-                    Intent(view.getContext(),
-                    RecentBuysActivity.class);
-            String message = "";
-            intent.putExtra("MESSAGE", message);
-            view.getContext().startActivity(intent);
-        } if (view.getId() == R.id.buttonShoppingList ) {
-            Intent intent = new
-                    Intent(view.getContext(),
-                    ShoppingListActivity.class);
-            String message = "";
-            intent.putExtra("MESSAGE", message);
-            view.getContext().startActivity(intent);
+    private class ButtonClickListener implements
+            View.OnClickListener
+    {
+        /**
+         *
+         * @param view View in which the buttons are
+         */
+        @Override
+        public void onClick( View view ) {
+            if (view.getId() == R.id.buttonRecentBuys ) {
+                Intent intent = new
+                        Intent(view.getContext(),
+                        RecentBuysActivity.class);
+                String message = "";
+                intent.putExtra("MESSAGE", message);
+                view.getContext().startActivity(intent);
+            } else if (view.getId() == R.id.buttonShoppingList ) {
+                Intent intent = new
+                        Intent(view.getContext(),
+                        ShoppingListActivity.class);
+                String message = "";
+                intent.putExtra("MESSAGE", message);
+                view.getContext().startActivity(intent);
+            } else if (view.getId() == R.id.buttonLogout) {
+                finish();
+            }
+
         }
-
     }
 }
